e3b8705e377c91d20ef528ee876d1922b4c98c02
shreya05-j <shreyajaiswal0575@gmail.com>
2026-02-03 18:03:50 +0530

    init: setup react + vite project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontSize: "18px" }}>
+      Code Editor Project Started
+    </div>
+  );
+}
+
+export default App;
